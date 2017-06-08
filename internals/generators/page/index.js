50a394044a345fd6e68ecae6747f14add5b50e3e
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-08 14:52:10 +0200

    fixes couple of small bugs into generators

--- internals/generators/page/index.js
+++ internals/generators/page/index.js
@@ -29,7 +29,7 @@ module.exports = {
 
     const actions = [{
       type: 'add',
-      path: '../../pages/{{properCase name}}.js',
+      path: '../../pages/{{name}}.js',
       templateFile: pageTemplate,
       abortOnFail: true,
     }];
