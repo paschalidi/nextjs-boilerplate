b6eca3b2b08ef9e911ee713b4b37ea13e5fd6f78
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-08 14:20:00 +0200

    implement Layout component

--- components/Layout/tests/index.test.js
+++ components/Layout/tests/index.test.js
@@ -1,9 +1,9 @@
 // import React from 'react';
 // import { shallow } from 'enzyme';
 
-// import Button from '../index';
+// import Layout from '../index';
 
-describe('<Button />', () => {
+describe('<Layout />', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
   });
